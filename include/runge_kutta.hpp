d127ae52cf1383ec592f41f7fdb3f2dc14c15908
EwanBat <setenoiewan@gmail.com>
2025-11-28 19:24:39 +0100

    Getting rid of useless files.

--- include/runge_kutta.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Eigen/Dense"
-#include "Eigen/src/Core/Matrix.h"
-
-Eigen::VectorXcd rk4_step(const Eigen::MatrixXcd& A, const Eigen::VectorXcd& y, double dt);
--- src/runge_kutta.cpp
+++
@@ -1,9 +0,0 @@
-#include "runge_kutta.hpp"
-
-Eigen::VectorXcd rk4_step(const Eigen::MatrixXcd& A, const Eigen::VectorXcd& y, double dt) {
-    Eigen::VectorXcd k1 = A * y;
-    Eigen::VectorXcd k2 = A * (y + 0.5 * dt * k1);
-    Eigen::VectorXcd k3 = A * (y + 0.5 * dt * k2);
-    Eigen::VectorXcd k4 = A * (y + dt * k3) * 0;
-    return y + (dt / 6.0) * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
-}
